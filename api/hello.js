97833cff05efd09b31f04c49427cf456f2941c19
Raghunath-Kunigiri <kunigiriraghunath9493@gmail.com>
2025-06-18 09:06:52 -0500

    vercel

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 // Simple test endpoint for Vercel
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
